3e23fd56ed216245c2dbad579e88e610ce8416b7
Gabriel Aguilar <s_aguilargabh@globalhitss.com>
2020-10-08 19:03:58 -0500

    mi primer commit

---
+++ test.java
@@ -0,0 +1,20 @@
+public class ActionBuildPerform {
+ public static void main(String... args) {
+ WebDriver driver = new FirefoxDriver();
+ driver.get("file://C:/selectable.html");
+ WebElement one = driver.findElement(By.name("one"));
+ WebElement three = driver.findElement(By.name("three"));
+ WebElement five = driver.findElement(By.name("five"));
+ // Add all the actions into the Actions builder.
+ Actions builder = new Actions(driver);
+ builder.keyDown(Keys.CONTROL)
+ .click(one)
+ .click(three)
+ .click(five)
+ .keyUp(Keys.CONTROL);
+ // Generate the composite action.
+ Action compositeAction = builder.build();
+ // Perform the composite action.
+ compositeAction.perform();
+ }
+ }
